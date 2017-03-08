96af21dd994571dbd446d137ad276c05324596fd
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-03-08 15:29:43 -0800

    Separation of files

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,2 +1,27 @@
-$('.navbar').localScroll();
+      $(document).ready(function(){
+      	var scroll_start = 0;
+      	var startchange = $('#headerwrap');
+      	var offset = startchange.offset();
+      	if (startchange.length){
+      		$(document).scroll(function() {
+      			scroll_start = $(this).scrollTop();
+      			if(scroll_start > offset.top) {
+      				$(".navbar-default").css('background-color', 'transparent');
+      			}
+      		});
+      	}
+      });
 
+      $(document).ready(function(){
+      	var scroll_start = 0;
+      	var startchange = $('#recentProjects');
+      	var offset = startchange.offset();
+      	if (startchange.length){
+      		$(document).scroll(function() {
+      			scroll_start = $(this).scrollTop();
+      			if(scroll_start > offset.top) {
+      				$(".navbar-default").css('background-color', '#ffffff');
+      			}
+      		});
+      	}
+      });
