9c21975aaa15339506ace93294d51ab4660317df
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-03-08 16:17:44 -0800

    added parallax

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,27 +1,37 @@
-      $(document).ready(function(){
-      	var scroll_start = 0;
-      	var startchange = $('#headerwrap');
-      	var offset = startchange.offset();
-      	if (startchange.length){
-      		$(document).scroll(function() {
-      			scroll_start = $(this).scrollTop();
-      			if(scroll_start > offset.top) {
-      				$(".navbar-default").css('background-color', 'transparent');
-      			}
-      		});
-      	}
-      });
+$('.navbar').localScroll();
 
-      $(document).ready(function(){
-      	var scroll_start = 0;
-      	var startchange = $('#recentProjects');
-      	var offset = startchange.offset();
-      	if (startchange.length){
-      		$(document).scroll(function() {
-      			scroll_start = $(this).scrollTop();
-      			if(scroll_start > offset.top) {
-      				$(".navbar-default").css('background-color', '#ffffff');
-      			}
-      		});
-      	}
-      });
+$(document).ready(function(){
+	var scroll_start = 0;
+	var startchange = $('#headerwrap');
+	var offset = startchange.offset();
+	if (startchange.length){
+		$(document).scroll(function() {
+			scroll_start = $(this).scrollTop();
+			if(scroll_start > offset.top) {
+				$(".navbar-default").css('background-color', 'transparent');
+			}
+		});
+	}
+});
+
+$(document).ready(function(){
+	var scroll_start = 0;
+	var startchange = $('#recentProjects');
+	var offset = startchange.offset();
+	if (startchange.length){
+		$(document).scroll(function() {
+			scroll_start = $(this).scrollTop();
+			if(scroll_start > offset.top) {
+				$(".navbar-default").css('background-color', '#ffffff');
+			}
+		});
+	}
+});
+
+$('.parallax-window').parallax({imageSrc: 'assets/img/wallpaperNature.jpg'});
+
+$('.parallax-window-about').parallax({imageSrc: 'assets/img/aboutBW.jpg'});
+
+
+
+
